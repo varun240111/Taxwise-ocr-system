3d2f5b56476976b926629b8c16429bb3e9762b13
varun240111 <varunpawar240@gmail.com>
2026-06-19 13:21:57 +0530

    Frontend production API config

--- taxwise_Frontend/src/services/api.js
+++ taxwise_Frontend/src/services/api.js
@@ -3,7 +3,7 @@ import { store } from "../store/store";
 import { setCredentials, logout } from "../store/authSlice";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
   withCredentials: true,
 });
 
