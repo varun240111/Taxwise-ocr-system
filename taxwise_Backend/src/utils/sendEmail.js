0d74e6bf4e5137c6ffcf4a83f4192f24c1b9993f
varun240111 <varunpawar240@gmail.com>
2026-06-19 11:48:28 +0530

    Corrections

--- taxwise_Backend/src/utils/sendEmail.js
+++ taxwise_Backend/src/utils/sendEmail.js
@@ -1,17 +1,19 @@
 import nodemailer from "nodemailer";
-    
-const sendEmail=async ({to,subject,html})=>{
-    const transporter=nodemailer.createTransport({
-        service:"gmail",
 
-        auth:{
-            user:process.env.EMAIL_USER,
-            pass:process.env.EMAIL_PASS,
+const sendEmail = async ({ to, subject, html }) => {
+
+
+
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
         },
     });
 
     await transporter.sendMail({
-        from:`"Taxwise Valut" <${process.env.EMAIL_USER}>`,
+        from: `"Taxwise Vault" <${process.env.EMAIL_USER}>`,
         to,
         subject,
         html,
